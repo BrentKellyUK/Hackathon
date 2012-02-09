eca7bd9b5ee1543e268757374da6bce3d6c43f14
algard <david.algar@mindbodyonline.com>
2012-02-09 01:53:22 +0000

    node test

--- jQueryPlugin/src/explodingList.js
+++ jQueryPlugin/src/explodingList.js
@@ -16,7 +16,21 @@
                             }
             );
     };
+    
+    $.fn.addDataToMap = function (methodName,argList){
+        
+    }
 
+})( jQuery );
+
+//TODO let the inputs start hidden, and use this method to un-hide them
+function createNewCall(method){
+    var newContent = "<input class='argValueInput' type='text' name='appointmentIDs' value=' ' />";
+    $("#"+method).find(".argument").each( function (){
+            $(this).append(newContent);
+        }
+        );
+}
 
 
 function showOrHideChildren(id){
@@ -24,4 +38,49 @@ function showOrHideChildren(id){
     id.parent().children("ul").toggleClass('hidden');
 }
 
-})( jQuery );
+function addAllTestsToMethod(methodName){
+    
+}
+
+
+function addTestToMethod(method, argList){
+   var testList = $("*[name="+method+"]").find(".apiTestList");
+   
+   
+   
+   testList.append(makeListElementFromArgumentList(argList));
+   
+}
+
+// creates the argument list <li> to show the 'test' run
+function makeListElementFromArgumentList(argList){                        
+    var result=  "<li class='apiTest'> ";
+    var flag = false;
+    for(argument in argList){
+        if(!flag){
+            flag = true;   
+        }else{
+            result += ", ";
+        }        
+        result += "<span class='apiTestArgument'>"+arg.name+"</span> &#8594; "+arg.value;
+    }
+    result += "</li>";
+    return result;
+}
+
+/**** sample json file: 
+ 
+var allArgLists = { ....  }; //JSON data
+
+(function(){
+
+    var list = parseListFromJSON(allArgLists);
+
+    for(argList in AllArgLists){
+        addDataToMap();
+    }      
+});
+
+
+
+***/
